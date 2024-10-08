541939e3fc13aae8c793f7f86e00b818053a1639
Nimren Naiju <72309229+nimren9920@users.noreply.github.com>
2024-10-08 06:20:45 +0000

    browserstack integrated

--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,22 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import LoginForm from './LoginForm'; // Assuming the component is named LoginForm
+const { Builder, By, Capabilities } = require("selenium-webdriver");
 
-describe('LoginForm Component', () => {
-  it('should allow entering email/username and password, and submit the form', () => {
-    const mockSubmitFunction = jest.fn(); // Mock function for form submission
+describe("ProLearning Test", () => {
+  let driver;
 
-    render(<LoginForm onSubmit={mockSubmitFunction} />);
-
-    // Find input fields
-    const emailInput = screen.getByPlaceholderText(/Enter your email or username/i);
-    const passwordInput = screen.getByPlaceholderText(/Enter a Password/i);
-    const loginButton = screen.getByRole('button', { name: /login/i });
-
-    // Simulate user typing email and password
-    fireEvent.change(emailInput, { target: { value: 'test.student@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'password123' } });
-
-    // Simulate form submission by clicking the login button
-    fireEvent.click(loginButton);
-
-    // Check if the mock submit function was called with the correct credentials
-    expect(mockSubmitFunction).toHaveBeenCalledWith({
-      email: 'test.student@edxample.com',
-      password: 'password123',
-    });
+  beforeAll(() => {
+    driver = new Builder()
+      .usingServer(`https://prolearning-frontend.vercel.app/login`)
+      .withCapabilities(Capabilities.chrome())
+      .build();
   });
+  
+  afterAll(async () => {
+    await driver.quit();
+  })
+
+  test("local test", async () => {
+    await driver.get("https://prolearning-frontend.vercel.app/login");
+
+    expect(await driver.getTitle()).toContain('ProLearning');
+  }, 10000000);
 });
